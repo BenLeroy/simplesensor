7704c9f4fc123aa70436684391ac48d33870ad7f
BenLeroy <bleroy@live>
2015-06-12 11:54:54 +0200

    Automatic mails are sent with events summary

--- plugins/send-mail.js
+++ plugins/send-mail.js
@@ -1,20 +1,22 @@
 var dsConfig = require('../server/datasources.json');
 
 module.exports = function(content, app) {
-  var yourEmailAddress = process.env.MAIL_ALERT;
 
-  if (yourEmailAddress !== "") {
+  if (process.env.MAIL_ALERT) {
 
     app.models.Email.send({
-      to: yourEmailAddress
+      to: process.env.MAIL_ALERT
       , from: "noreply@denaroo.com"
       , subject: 'Changements d\'états pour le dernier interval'
       , text: content
     }
     , function(err) {
-      if (err) throw err;
-        console.log('> email sent successfully');
-    });  
+      if (err) {
+        throw err;
+      }
+
+      console.log('> email sent successfully');
+    });
   }
   else {
     console.log('No email sent since mail adress (ENV MAIL_ALERT) is not set');
