4d9f3e85bdeab8399a2857724ad565f1c1c9cada
BenjaminL <benjamin.leroy@denaroo.com>
2016-08-09 13:28:28 +0200

    Fix inCheck route

--- common/models/sensor.js
+++ common/models/sensor.js
@@ -1,54 +1,59 @@
 module.exports = function(Sensor) {
 
-	Sensor.inCheck = function (key, status, cb) {
+  Sensor.inCheck = function (key, status, cb) {
 
-		var haveChanged = false;
+    var haveChanged = false;
 
-		if(key === undefined || key === "") {
-			return cb('Key cannot be blank');
-		}
+    if(key === undefined || key === "") {
+      return cb('Key cannot be blank');
+    }
 
-		Sensor.findOrCreate({where: {key: key}}
-			, {key: key
-			, name: key + ' non configuré'
-			, createdAt: Date.now()
-			, frequency: 60
-			, lag: 1500}
-			, function (err, instance){
+    Sensor.findOrCreate({where: {key: key}}
+      , {key: key
+      , name: key
+      , status: status
+      , createdAt: Date.now()
+      , frequency: 60
+      , lag: 1500}
+      , function (err, instance){
 
-		        Sensor.upsert(instance, function (err, obj) {
+        Sensor.upsert(instance, function (err, obj) {
 
-					obj.modifiedAt = Date.now();
-					obj.checkedAt = Date.now();
+          if (err) {
+            console.log("Unable to update sensor ", err);
+          }
 
-			        if (instance.__data.status !== status) {
-			          obj.status = status;
-			          haveChanged = true;
-			        }
-					obj.save({}, function (ert, objt) {
-			          if (haveChanged) {
-			            Sensor.emit('sensor:' + status, obj);
-			          }
-						cb(
-							null
-							, objt
-						);
-					});
-				});
-			}
-		);
-	};
+          obj.modifiedAt = Date.now();
+          obj.checkedAt = Date.now();
 
-	Sensor.remoteMethod('inCheck',
-		{
-			accepts: [
-				{arg: 'key', type: 'string'}
-				, {arg: 'status', type: 'string'}
-				],
-			returns: {arg: 'sensor', type: 'object'},
-			http: {path: '/inCheck'},
-			description: "Update existing model instance or insert a new one if it doesn't exist"
+          if (instance.__data.status !== status) {
+            obj.status = status;
+            haveChanged = true;
+          }
+          obj.save({}, function (ert, objt) {
+            if (haveChanged) {
+              Sensor.emit('sensor:' + status, obj);
+            }
+            cb(
+              null
+              , objt
+            );
+          });
+        });
+      }
+    );
+  };
 
-		}
-	);
+  Sensor.remoteMethod('inCheck',
+    {
+      accepts: [
+        {arg: 'key', type: 'string'}
+        , {arg: 'status', type: 'string'}
+        ],
+      returns: {arg: 'sensor', type: 'object'},
+      http: {path: '/inCheck'},
+      description: "Update existing model instance or insert a new one if it doesn't exist"
+
+    }
+  );
 };
