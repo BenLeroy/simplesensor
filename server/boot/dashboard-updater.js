ea439196306688000f0266aedc428b2f99b304b0
BenjaminL <benjamin.leroy@denaroo.com>
2016-09-30 12:16:05 +0200

    fix(dash): move query to amqp listener

--- server/boot/dashboard-updater.js
+++ server/boot/dashboard-updater.js
@@ -7,38 +7,22 @@ module.exports = function(app, cb) {
 
   app.on('started', function(){
 
-    function newSensorEvent() {
-      // get 5 first NOK/missing sensors
-      sensors.find(
-        { 
-          filter: {
-            limit: 5
-            , where:{
-              status: {
-                inq: ['NOK', 'Missing']
-              }
-            }
-          }
-        }
-      , function (err, data) {
-        if (err) {throw err;}
-        // send them to dashing using amqp/RabbitMQ
-        amqp.connect(
-          'amqp://' + process.env.RABBIT_URL
-          , function(conErr, conn) {
-            if (conErr) {throw conErr}
-            conn.createChannel(function(chErr, ch) {
-              if (chErr) {throw chErr}
-              var ex = 'SIMPLESENSOR';
-              var msg = JSON.stringify(data);
+    function newSensorEvent(probe) {
+      amqp.connect(
+        'amqp://' + process.env.RABBIT_URL
+        , function(conErr, conn) {
+          if (conErr) {throw conErr}
+          conn.createChannel(function(chErr, ch) {
+            if (chErr) {throw chErr}
+            var ex = 'SIMPLESENSOR';
+            var msg = JSON.stringify(probe);
 
-              ch.assertExchange(ex, 'direct', {durable: false});
-              ch.publish(ex, 'NEW_SENSORS', new Buffer(msg));
-              console.log(" [x] Sent updated data to dashboard");
-            });
-          }
-        );
-      });
+            ch.assertExchange(ex, 'direct', {durable: false});
+            ch.publish(ex, 'NEW_SENSORS', new Buffer(msg));
+            console.log(" [x] Sent updated data to dashboard");
+          });
+        }
+      );
     }
     if (process.env.RABBIT_URL) {
       sensors.on('sensor:NOK'     , newSensorEvent);
