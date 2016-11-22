b95b5ea6793c8a2fa3734a31c9511ae596a6a4dd
BenjaminL <benjamin.leroy@denaroo.com>
2016-11-22 16:48:29 +0100

    fix(simple): change amqp connection management

--- server/boot/dashboard-updater.js
+++ server/boot/dashboard-updater.js
@@ -4,32 +4,36 @@ module.exports = function(app, cb) {
   var sensors = app.models.Sensor;
 
   var amqp = require('amqplib/callback_api');
+  var amqpConn = null;
 
   app.on('started', function(){
+    amqp.connect(
+      'amqp://' + process.env.RABBIT_URL
+      , function(conErr, conn) {
+        if (conErr) {throw conErr}
+        amqpConn = conn;
+      }
+    );
 
     function newSensorEvent(probe) {
-      amqp.connect(
-        'amqp://' + process.env.RABBIT_URL
-        , function(conErr, conn) {
-          if (conErr) {throw conErr}
-          conn.createChannel(function(chErr, ch) {
-            if (chErr) {throw chErr}
-            var ex = 'SIMPLESENSOR';
-            var msg = JSON.stringify(probe);
+      if (amqpConn) {
+        amqpConn.createChannel(function(chErr, ch) {
+          if (chErr) {throw chErr}
+          var ex = 'SIMPLESENSOR';
+          var msg = JSON.stringify(probe);
 
-            ch.assertExchange(ex, 'direct', {durable: false});
-            ch.publish(ex, 'NEW_SENSORS', new Buffer(msg));
-            console.log(" [x] Sent updated data to dashboard");
-          });
-        }
-      );
+          ch.assertExchange(ex, 'direct', {durable: false});
+          ch.publish(ex, 'NEW_EVENT', new Buffer(msg));
+          console.log(" [x] Sent updated data to dashboard");
+        });
+      }
     }
+
     if (process.env.RABBIT_URL) {
       sensors.on('sensor:NOK'     , newSensorEvent);
       sensors.on('sensor:OK'      , newSensorEvent);
       sensors.on('sensor:Missing' , newSensorEvent);
     }
-
   });
   cb(null);
 };
