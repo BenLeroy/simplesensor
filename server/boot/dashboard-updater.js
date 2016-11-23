4132f9c1975d83ae80396066f81b988b8d0dd841
BenjaminL <benjamin.leroy@denaroo.com>
2016-11-23 14:11:33 +0100

    fix(simple): move rabbit url test and add /update route for dashboard

--- server/boot/dashboard-updater.js
+++ server/boot/dashboard-updater.js
@@ -7,29 +7,34 @@ module.exports = function(app, cb) {
   var amqpConn = null;
 
   app.on('started', function(){
-    amqp.connect(
-      'amqp://' + process.env.RABBIT_URL
-      , function(conErr, conn) {
-        if (conErr) {throw conErr}
-        amqpConn = conn;
-      }
-    );
-
-    function newSensorEvent(probe) {
-      if (amqpConn) {
-        amqpConn.createChannel(function(chErr, ch) {
-          if (chErr) {throw chErr}
-          var ex = 'SIMPLESENSOR';
-          var msg = JSON.stringify(probe);
-
-          ch.assertExchange(ex, 'direct', {durable: false});
-          ch.publish(ex, 'NEW_EVENT', new Buffer(msg));
-          console.log(" [x] Sent updated data to dashboard");
-        });
-      }
-    }
-
     if (process.env.RABBIT_URL) {
+      amqp.connect(
+        'amqp://' + process.env.RABBIT_URL
+        , function(conErr, conn) {
+          if (conErr) {throw conErr}
+          amqpConn = conn;
+        }
+      );
+
+      function newSensorEvent(probe) {
+        if (amqpConn) {
+          amqpConn.createChannel(function(chErr, ch) {
+            if (chErr) {throw chErr}
+            var ex = 'SIMPLESENSOR';
+            var msg = JSON.stringify(probe);
+
+            ch.assertExchange(ex, 'direct', {durable: false});
+            ch.publish(ex, 'NEW_EVENT', new Buffer(msg));
+            console.log(" [x] Sent updated data to dashboard");
+          });
+        }
+      }
+      
+      app.get('/update', function(req, res) {
+        newSensorEvent('update');
+        res.send('OK');
+      });
+
       sensors.on('sensor:NOK'     , newSensorEvent);
       sensors.on('sensor:OK'      , newSensorEvent);
       sensors.on('sensor:Missing' , newSensorEvent);
